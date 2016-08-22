4fd46e7607f73dc0e6d19cf3605dd3aad210f903
vitorhugos <vitorhugoss17@gmail.com>
2016-08-22 17:53:13 -0300

    Modulo 1

--- PHP e MySQL I: Fundamentos para criar um sistema na Web/adiciona-produto.php
+++ PHP e MySQL I: Fundamentos para criar um sistema na Web/adiciona-produto.php
@@ -1,16 +1,22 @@
 <?php
 include "cabecalho.php";
+function insereProduto($conexao,$nome, $preco){
+	$query = "insert into produtos (nome, preco) values ('{$nome}',{$preco})";
+	return mysqli_query($conexao, $query);
+}
+
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
 $conexao = mysqli_connect("localhost", "root", "", "loja");
-$query = "insert into produtos (nome, preco) values ('{$nome}',{$preco})";
-if(mysqli_query($conexao, $query)){
+
+if(insereProduto($conexao, $nome, $preco)){
 ?>
-	<p class="alert-success">O produto <?=$nome?>, <?=$preco?> foi adicionado.</p>
+	<p class="text-success">O produto <?=$nome?>, <?=$preco?> foi adicionado.</p>
 <?php
 }else{
+$msg = mysqli_error($conexao);
 ?>
-	<p class="alert-danger">O produto <?=$nome?> não foi adicionado.</p>
+	<p class="text-danger">O produto <?=$nome?> não foi adicionado: <?=$msg?></p>
 <?php
 }
 mysqli_close($conexao);
--- PHP e MySQL I: Fundamentos para criar um sistema na Web/cabecalho.php
+++ PHP e MySQL I: Fundamentos para criar um sistema na Web/cabecalho.php
@@ -15,6 +15,18 @@
 </style>
 </head>
 <body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a href="index.php" class="navbar-brand">Minha loja</a>
+			</div>
+			<div>
+			<ul class="nav navbar-nav">
+				<li><a href="produto-formulario.php">Adicionar Produto</a></li>
+				<li><a href="sobre.php">Sobre</a></li>
+			</ul>
+		</div>
+		</div>
+	</div>
     <div class="container">
-
         <div class="principal">
